2e70edd4bc0349123c4a825e1dd5c22978d195b3
unknown <infinity_michael@yahoo.com>
2020-03-28 10:22:34 -0700

    added react router

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import Home from "../pages/Home";
-import Header from '../components/Header/Header';
+import Header from "../components/Header/Header";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const Layouts: React.FC = () => {
+interface RouteProps {
+	// location?: H.Location;
+	// component?: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
+	// render?: ((props: RouteComponentProps<any>) => React.ReactNode);
+	// children?: ((props: RouteChildrenProps<any>) => React.ReactNode) | React.ReactNode;
+	path?: string | string[];
+	exact?: boolean;
+	sensitive?: boolean;
+	strict?: boolean;
+	Component?: React.FC<any> | null;
+}
+
+const Layouts: React.FC<RouteProps> = () => {
 	return (
-		<div>
-            <Header/>
-			<Home />
-		</div>
+		<Router>
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/create-account" component={Register} />
+				</Switch>
+			</div>
+		</Router>
 	);
 };
 
