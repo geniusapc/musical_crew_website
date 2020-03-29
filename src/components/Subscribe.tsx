d78185d23317c3ff872f654bde36c4c1f1c831f7
unknown <infinity_michael@yahoo.com>
2020-03-29 12:23:39 -0700

    created and updated the subscribe button component

---
+++ src/components/Subscribe.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props {}
+
+const Subscribe: React.FC<Props> = () => {
+	return (
+		<div className="subscribe">
+            <h5><span> WANT TO RECEIVE</span> OUR UPDATES</h5>
+			<div className="subscribe-search">
+				<input type="text" placeholder="Enter your email" />
+				<span>SUBSCRIBE</span>{" "}
+			</div>
+		</div>
+	);
+};
+
+export default Subscribe;
