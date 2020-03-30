a2893815652298888d13cf23ce3ad1445bc73ad7
unknown <infinity_michael@yahoo.com>
2020-03-30 09:25:19 -0700

    updated the section components

--- src/components/landingpage/section-a/HotRelease.tsx
+++ src/components/landingpage/section-a/HotRelease.tsx
@@ -1,32 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
+import { useSelector,TypedUseSelectorHook } from "react-redux";
 import HotReleaseList from "./HotReleaseList";
 
-// interface Props {}
-
-// interface DataTypes {
-// 	id: number;
-// 	title: string;
-// 	duration: string;
-// 	watched?: boolean;
-// }
-
-// type State = DataTypes[];
-
+interface RootState {
+	Music: Array<{
+		id:number;
+		title:string;
+		duration:string;
+	}>;
+  }
+  
+  
 const HotRelease: React.FC = () => {
-	const [newMusic, setNewMusic] = useState([
-		{ id: 1, title: "new song for the soul", duration: "4:52" },
-		{ id: 2, title: "new song for the soul", duration: "4:52" },
-		{ id: 3, title: "new song for the soul", duration: "4:52" },
-		{ id: 4, title: "new song for the soul", duration: "4:52" },
-		{ id: 5, title: "new song for the soul", duration: "4:52" },
-		{ id: 6, title: "new song for the soul", duration: "4:52" },
-		{ id: 7, title: "new song for the soul", duration: "4:52" },
-		{ id: 8, title: "new song for the soul", duration: "4:52" }
-	]);
-
+	const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+	const newMusicList = useTypedSelector(state => state.Music);
 	return (
 		<div className="wrapper">
-			{newMusic.map(music => (
+			{newMusicList.map(music => (
 				<div key={music.id}>
 					<HotReleaseList
 						title={music.title}
--- src/components/landingpage/section-a/HotReleaseList.tsx
+++ src/components/landingpage/section-a/HotReleaseList.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+// import musicListActions from "../../../redux-store/actions/LatestMusic";
+import {GetSpecificID} from "../../../redux-store/actions/LatestMusic";
 
 interface Props {
 	title: string;
 	serialno: number;
 	duration: string;
 	link?: string;
-}
+	// onClick?:
+	// 	| ((event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void)
+	// 	| undefined;
+};
 
-
-const HotReleaseList: React.FC <Props>= ({serialno,title,duration}) => {
+const HotReleaseList: React.FC<Props> = ({ serialno, title, duration }) => {
+	const dispatch = useDispatch();
 	return (
-		<div className="music-list">
+		<div className="music-list" onClick={()=>(dispatch(GetSpecificID(serialno)))}>
 			<span>{serialno}</span>
 			<span>{title}</span>
 			<span>{duration}</span>
@@ -19,3 +25,5 @@ const HotReleaseList: React.FC <Props>= ({serialno,title,duration}) => {
 };
 
 export default HotReleaseList;
+
+// 
