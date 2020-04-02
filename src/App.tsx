f980aeda183790acd697b8b42708d62c4a36ace6
unknown <infinity_michael@yahoo.com>
2020-04-02 11:02:27 -0700

    created custom material ui themes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,33 @@ import React from "react";
 import Layouts from "./layouts/Layouts";
 import './sass/main.scss'
 
+import { createMuiTheme ,ThemeProvider} from '@material-ui/core/styles';
+
+
+
+
+
+const theme = createMuiTheme({
+  palette: {
+	primary: {
+		main: '#d50000',
+	  },
+	secondary: {
+		main: '#202020',
+	  },
+  },
+ 
+
+});
+
 
 const App: React.FC = () => {
 	return (
+		<ThemeProvider theme={theme}>
 		<div>
 			<Layouts/>
 		</div>
+		</ThemeProvider>
 	);
 };
 
