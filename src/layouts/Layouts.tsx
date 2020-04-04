13070a6146901fffaab7be1170a2a50e7dc148ca
unknown <infinity_michael@yahoo.com>
2020-04-04 03:23:22 -0700

    updated the layout comp.

--- src/layouts/Layouts.tsx
+++ src/layouts/Layouts.tsx
@@ -3,61 +3,58 @@ import Home from "../pages/Home";
 import Header from "../components/Header/Header";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Galleries from '../pages/GalleryGridList';
+import Galleries from "../pages/GalleryGridList";
 import Footer from "../components/Footer";
-import ViewEvents from '../components/landingpage/section-b/ViewEvent';
+import ViewEvents from "../pages/ViewEvent";
+import ContactPage from '../pages/ContactPage';
+import ErrorPage from '../pages/ErrorPage';
 
-
-
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useSelector, TypedUseSelectorHook, useDispatch } from "react-redux";
-import { CollapseSideBar, ToogleSideBar } from "../redux-store/actions/Toogle";
+import { BrowserRouter as Router, Switch, Route,Redirect } from "react-router-dom";
+// import { useSelector, TypedUseSelectorHook, useDispatch } from "react-redux";
+// import { CollapseSideBar, ToogleSideBar } from "../redux-store/actions/Toogle";
 
 interface RouteProps {
-	// location?: H.Location;
-	// component?: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any>;
-	// render?: ((props: RouteComponentProps<any>) => React.ReactNode);
-	// children?: ((props: RouteChildrenProps<any>) => React.ReactNode) | React.ReactNode;
-	path?: string | string[];
-	exact?: boolean;
-	sensitive?: boolean;
-	strict?: boolean;
-	Component?: React.FC<any> | null;
+	// path?: string | string[];
+	// exact?: boolean;
+	// sensitive?: boolean;
+	// strict?: boolean;
+	// component?: React.FC<any> | null;
 }
 
-interface RootState {
-	Toggle: Boolean;
-}
+// interface RootState {
+// 	Toggle: Boolean;
+// }
 
 const Layouts: React.FC<RouteProps> = () => {
-	const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
-	let Toggle = useTypedSelector(state => state.Toggle);
-	const dispatch = useDispatch();
-	// const toggleNavBar = () =>
-	// 	(Toggle = false ? dispatch(CollapseSideBar()) : true);
-	const toggleNavBar = () =>{
-		//  (dispatch(CollapseSideBar()));
-		if (Toggle = false) {
-			dispatch(ToogleSideBar())
-			// Toggle =false;
-			// console.log(Toggle);
-			
-		} else {
-			return;;
-		}
-	}
+	// const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+	// let Toggle = useTypedSelector(state => state.Toggle);
+	// const dispatch = useDispatch();
+	// // const toggleNavBar = () =>
+	// // 	(Toggle = false ? dispatch(CollapseSideBar()) : true);
+	// const toggleNavBar = () =>{
+	// 	//  (dispatch(CollapseSideBar()));
+	// 	if (Toggle = false) {
+	// 		dispatch(ToogleSideBar())
+	// 		// Toggle =false;
+	// 		// console.log(Toggle);
+
+	// 	} else {
+	// 		return;;
+	// 	}
+	// }
 	return (
 		<Router>
-			<div onClick={toggleNavBar}>
+			<div>
 				<Header />
 				<Switch>
 					<Route exact path="/" component={Home} />
 					<Route exact path="/our-galleries" component={Galleries} />
 					<Route exact path="/login" component={Login} />
-					
 					<Route exact path="/create-account" component={Register} />
 					<Route exact path="/events" component={ViewEvents} />
+					<Route exact path="/contact-us" component={ContactPage} />
+					<Route exact path="/page-not-found" component={ErrorPage} />
+					<Redirect to="/page-not-found"/>
 				</Switch>
 				<Footer />
 			</div>
