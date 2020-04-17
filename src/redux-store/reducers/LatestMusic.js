b94005cea97c3bd4f078f94c22df107213374ecf
unknown <infinity_michael@yahoo.com>
2020-04-16 17:50:05 -0700

    updated  reduz store

--- src/redux-store/reducers/LatestMusic.js
+++ src/redux-store/reducers/LatestMusic.js
@@ -1,45 +1,46 @@
 export const MusicListReducer = (
-    state = [
-        { id: 1, title: "new song for the soul", duration: "4:52" },
-        { id: 2, title: "new song for the soul", duration: "4:52" },
-        { id: 3, title: "new song for the soul", duration: "4:52" },
-        { id: 4, title: "new song for the soul", duration: "4:52" },
-        { id: 5, title: "new song for the soul", duration: "4:52" },
-        { id: 6, title: "new song for the soul", duration: "4:52" },
-        { id: 7, title: "new song for the soul", duration: "4:52" },
-        { id: 8, title: "new song for the soul", duration: "4:52" }
-    ],
-    action
+  state = [
+    { id: 1, title: "put it on me", arthur: "Ed sheeran", link: "https://www.youtube.com/watch?v=mj0XInqZMHY&pbjreload=10" },
+    { id: 2, title: "Intensions", arthur: "justin bieber", link: "https://www.youtube.com/watch?v=3AyMjyHu1bA" },
+    { id: 3, title: "Memories", arthur: "Marron 5", link: "https://www.youtube.com/watch?v=SlPhMPnQ58k" },
+    { id: 4, title: "Bad Liars", arthur: "imagine dragons", link: "https://www.youtube.com/watch?v=I-QfPUz1es8" },
+    { id: 5, title: "Falling", arthur: "Trevor Daniel", link: "https://www.youtube.com/watch?v=L7mfjvdnPno" },
+    { id: 6, title: "What if i told you...", arthur: "Ali Gatie", link: "https://www.youtube.com/watch?v=NKzd_YiW9AQ" },
+    { id: 7, title: "Y.O.U", arthur: "Luh Kel", link: "https://www.youtube.com/watch?v=0y_2VU7x0hA" },
+    { id: 8, title: "Wanna be your man", arthur: "Mechie so crazy", link: "https://www.youtube.com/watch?v=VqZ6xrv5lFw" }
+  ],
+  action
 ) => {
-    switch (action.type) {
-        case "ADD":
-            return [...state, { id: Math.random(0, 100), text: "mike", title: "new boi enter", duration: "3:45" }];
-        case "GET_ID":
-            return state.filter((st) => st.id !== action.payload ? st : null);
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "ADD":
+      return [...state, { id: Math.random(0, 100), text: "mike", title: "new boi enter", duration: "3:45" }];
+    case "GET_ID":
+      return state.filter((st) => st.id !== action.payload ? st : null);
+    default:
+      return state;
+  }
 };
 
 export const EventState = (Events = [
-    {
-        id: 1,
-        title: "NewUpdate",
-        img: "https://source.unsplash.com/random",
-        text: `lorem30cdscbjhdscjhsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcjshbdcbjsdbsdbjjhvjdschvjvsdcjsvdjchvjdshv`
-    },
-    {
-        id: 2,
-        title: "Music concert",
-        img: "https://source.unsplash.com/random",
-        text: `lorem30cdscbjhdscjhsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcjshbdcbjsdbsdbjjhvjdschvjvsdcjsvdjchvjdshv`
-    },
-    {
-        id: 3,
-        title: "vacation with teams",
-        img: "https://source.unsplash.com/random",
-        text: `lorem30cdscbjhdscjhsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcsbdjhcbjshdcbjshdbcjhsdbjhcbsjhdbcjshbdcbjsdbsdbjjhvjdschvjvsdcjsvdjchvjdshv`
-    }
+  {
+    id: 1,
+    title: "NICOLE RICHIE'S RAP PERSONA ",
+    img: "https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:692035?height=729&width=1296&format=jpg&quality=.7",
+    text: `It’s an unusually rainy morning in Los Angeles, and Nicole Richie’s home garden is blooming. “Let's see, tomatoes are starting to come in, and sugar snap peas. We’ve got some eggplants, some garlic; rosemary and thyme, all the time,” she tells MTV News by phone, listing off what could be a near-complete grocery list. The 36-year-old House of Harlow designer and actress, who became a household name in the mid-2000s for her role alongside socialite-heiress Paris Hilton in the reality series The Simple Life, added amateur farmer to her extensive resume nearly a decade ago when she began cultivating a small plot with friends. “We started working on an edible garden, just one or two things, some strawberries, some blueberries,” she explains. “I just fell in love with it. It’s just so cool!” `
+  },
+  {
+    id: 2,
+    title: "KANYE WEST SAYS HE'S THE 'RAP VERSION' KOBE",
+    img: "https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:692036?height=729&width=1296&format=jpg&quality=.7",
+    text: `Kanye West and Kobe Bryant. Two legends with twice-in-a-generation work ethics and completely unpredictable thought processes.
+        Bryant's death in January profoundly affected West, as he recently revealed in GQ's May cover story. Inspired by the beloved five-time basketball champion, West explained why Bryant's memory keeps him going.`
+  },
+  {
+    id: 3,
+    title: "SEE SELENA GOMEZ LIKE YOU’VE NEVER SEEN",
+    img: "https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:691966?height=729&width=1296&format=jpg&quality=.7",
+    text: `Selena Gomez really can't believe that she has three number one albums. She's the new cover star of the latest edition of Interview magazine and, in a conversation with Amy Schumer, she revealed that the thought of her having several number one albums nearly didn't cross her mind. Chalk it up to her being one of the most humble denizens of the world we know as Hollywood. Her Interview magazine cover photoshoot shows a different side of her though. In it, she's boldly ready to take the world by storm via expensive car.`
+  }
 ]) => {
-    return Events;
+  return Events;
 };
