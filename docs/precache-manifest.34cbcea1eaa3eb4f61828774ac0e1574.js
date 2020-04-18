1aa7888ebb54992ebf9b0932a0f39030735e1803
unknown <infinity_michael@yahoo.com>
2020-04-17 19:38:05 -0700

    updated docs folder for github pages

---
+++ docs/precache-manifest.34cbcea1eaa3eb4f61828774ac0e1574.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb7bd003946f2b66e99969bcfadffc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08ed52b2f90525054bde",
+    "url": "/static/css/3.72d5056d.chunk.css"
+  },
+  {
+    "revision": "c3397ae758e20db5c73e",
+    "url": "/static/css/main.277a07cf.chunk.css"
+  },
+  {
+    "revision": "86018573e44ce8c1c07a",
+    "url": "/static/js/10.95c2ee78.chunk.js"
+  },
+  {
+    "revision": "a4744b7b0bbadf9d6a70",
+    "url": "/static/js/11.0553c03c.chunk.js"
+  },
+  {
+    "revision": "5f49cf641170cb89efd5",
+    "url": "/static/js/2.feab0071.chunk.js"
+  },
+  {
+    "revision": "bb07bf585b5364cfbb4632dd9308d60e",
+    "url": "/static/js/2.feab0071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ed52b2f90525054bde",
+    "url": "/static/js/3.8f6fe7c9.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/3.8f6fe7c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "376051d669c0804612aa",
+    "url": "/static/js/4.a05cc737.chunk.js"
+  },
+  {
+    "revision": "66bfcb5a1dba9b45cd3371b50d9d764d",
+    "url": "/static/js/4.a05cc737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4057737db0d5bdb50de3",
+    "url": "/static/js/5.cb7f23dc.chunk.js"
+  },
+  {
+    "revision": "dabb1af8777e4cdcc7cf",
+    "url": "/static/js/6.2f04c9e2.chunk.js"
+  },
+  {
+    "revision": "267d824dcd79ad461b1e",
+    "url": "/static/js/7.0ec684f8.chunk.js"
+  },
+  {
+    "revision": "d6d97c45deda36c3dbc7",
+    "url": "/static/js/8.66fa5c6d.chunk.js"
+  },
+  {
+    "revision": "4770a353c0666b39df65",
+    "url": "/static/js/9.ad3b3c2c.chunk.js"
+  },
+  {
+    "revision": "c3397ae758e20db5c73e",
+    "url": "/static/js/main.20e31617.chunk.js"
+  },
+  {
+    "revision": "3f73d9fe9935e8b4b3c7",
+    "url": "/static/js/runtime-main.1adc6700.js"
+  },
+  {
+    "revision": "3466e62e4ebc2c1898216186a32d0fb4",
+    "url": "/static/media/lady-sings-the-blues.3466e62e.jpg"
+  },
+  {
+    "revision": "c8ace92deff552b84fec9e1ae33444f1",
+    "url": "/static/media/logo.c8ace92d.png"
+  },
+  {
+    "revision": "97d400c86be8435eaa7f39f736203b41",
+    "url": "/static/media/maninblack.97d400c8.jpg"
+  },
+  {
+    "revision": "cecf1f4756132f13579c2d83cf4290d3",
+    "url": "/static/media/slide4.cecf1f47.jpg"
+  },
+  {
+    "revision": "8393d4e6e9da3f28122f827c9dd1b3d8",
+    "url": "/static/media/slide5.8393d4e6.jpg"
+  },
+  {
+    "revision": "71688064a7abbda7498b2a1d13b7ffbd",
+    "url": "/static/media/slide6.71688064.jpg"
+  }
+]);
